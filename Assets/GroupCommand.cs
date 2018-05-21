5e38d21ab9aa9c7859198a08b6d003016e3a6501
uwx <cadrekucra@gmx.com>
2018-05-21 01:44:57 -0300

    initial unity commit

---
+++ Assets/GroupCommand.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+ 
+public static class GroupCommand
+{
+    [MenuItem("GameObject/Group Selected %g")]
+    private static void GroupSelected()
+    {
+        if (!Selection.activeTransform) return;
+        var go = new GameObject(Selection.activeTransform.name + " Group");
+        Undo.RegisterCreatedObjectUndo(go, "Group Selected");
+        go.transform.SetParent(Selection.activeTransform.parent, false);
+        foreach (var transform in Selection.transforms) Undo.SetTransformParent(transform, go.transform, "Group Selected");
+        Selection.activeGameObject = go;
+        go.AddComponent<UnrootChildrenAndDeleteOnRunTime>();
+    }
+    
+    public class UnrootChildrenAndDeleteOnRunTime : MonoBehaviour {
+        private void Awake() {
+            while (transform.childCount > 0) {
+                transform.GetChild(0).parent = null;
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform target;
+    private Transform position;
+    private NavMeshAgent myNavAgent;
+
+    public float RotationSmoothness;
+
+    private bool allowMovement = true;
+
+    // Use this for initialization
+    private void Start()
+    {
+        // get and cache refrences.
+        myNavAgent = this.GetComponent<NavMeshAgent>();
+        position = this.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // check movement
+        DoMovement();
+    }
+
+    private void DoMovement()
+    {
+        // if We are not allowing movement then return;
+        if (!allowMovement)
+            return;
+
+        // Update Movement
+        var tmoMotionVector = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * myNavAgent.speed;
+        myNavAgent.Move(tmoMotionVector);
+        myNavAgent.SetDestination(transform.position + tmoMotionVector);
+
+        // Update Rotation
+        //var desiredRotation = Quaternion.LookRotation(InputManager.MovementAxisVec3, Vector3.up);
+        //var smoothedRotation = Quaternion.Lerp(this.transform.rotation, desiredRotation, RotationSmoothness);
+        //this.transform.rotation = smoothedRotation;
+        
+        var mouse = Input.mousePosition;
+        mouse.z = 5.23f; //The distance between the camera and object
+        var screenPoint = Camera.main.WorldToScreenPoint(position.position);
+        var angle = Mathf.Atan2(mouse.y - screenPoint.y, mouse.x - screenPoint.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, (180 - angle) - 90, 0));
+
+    }
+}
